c7d06d3c7bf871803e04c204b9bbcc7496006a8d
asada <kenichi.asada.g@gmail.com>
2020-05-02 14:46:49 +0900

    inplement callbacks with useUffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = props => {
 
   const [state, setState] = useState(props)
   const {name, price} = state
+
+  // ----------------------------
+  // 値が変更になった際に、何かしらのeffect(効果)を
+  // 表現したい際に、ハンドリングするための仕組み⇨useEffect
+  // レンダリング後に呼び出される
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate')
+  })
+
+  useEffect(() => {
+    console.log('This is like componentDidMount')
+  }, [])
+
+  useEffect(() => {
+    console.log('This callback is for name only.')
+  }, [name])
+
+
   return (
     <>
       <p>現在の{name}は、{price}円です。</p>
