0f4679464931814446bf789c9bd00d27b417395e
asada <kenichi.asada.g@gmail.com>
2020-05-02 17:06:43 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -40,7 +40,6 @@ const events = (state = [], action) => {
             const event = {title: action.title, body: action.body}
             const length = state.length
             const id = length === 0 ? 1 : state[length - 1].id + 1
-            state.push(event)
             return [...state, { id, ...event }]
         case 'DELETE_EVENT':
             return state
