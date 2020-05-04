d88b504c6e081084ba9c29f3352a5ac0b810d456
asada <kenichi.asada.g@gmail.com>
2020-05-04 15:02:38 +0900

    create operationLogs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from 'react'
 
-import { CREATE_EVENT, DELETE_ALL_EVENT } from '../actions'
+import { CREATE_EVENT, DELETE_ALL_EVENT, ADD_OPERATION_LOG, DELETE_ALL_OPERATION_LOGS } from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const EventForm = () => {
     const {state, dispatch} = useContext(AppContext)
@@ -10,8 +11,18 @@ const EventForm = () => {
   
     const addEvent = e => {
       e.preventDefault()
-      dispatch({type: CREATE_EVENT, title, body})
+      dispatch({
+        type: CREATE_EVENT, 
+        title, 
+        body
+      })
   
+      dispatch({
+        type: ADD_OPERATION_LOG, 
+        description: 'イベントを作成しました。', 
+        operatedAd: timeCurrentIso8601()
+      })
+
       setTitle('')
       setBody('')
     }
@@ -20,6 +31,12 @@ const EventForm = () => {
       e.preventDefault()
       if (window.confirm('全てのイベントを削除してもよろしいですか？')) { 
         dispatch({type: DELETE_ALL_EVENT})
+
+        dispatch({
+          type: ADD_OPERATION_LOG,
+          description: '全てのイベントを削除しました。',
+          operatedAd: timeCurrentIso8601()
+        })
       }
     }
   
